425f71f8da6781337b7b6c3062c7bc2c3cc4e0c7
Nishanth BS <nishanth.star1@gmail.com>
2018-11-25 13:18:43 +0530

    Modified naive bayes classifier

--- 6-Text Clasification Naivebayes/NaiveBayes-condensed.py
+++ 6-Text Clasification Naivebayes/NaiveBayes-condensed.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import fetch_20newsgroups
 
+#print(fetch_20newsgroups(subset='train').target_names)
 categories = ['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
 training_data =fetch_20newsgroups(subset='train',categories=categories,shuffle=True)
 testing_data = fetch_20newsgroups(subset='test',categories=categories,shuffle=True)
@@ -10,22 +11,21 @@ print("tt",training_data.target[0])
 
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 countVectorizer = CountVectorizer()
-X_train_tf = countVectorizer.fit_transform(training_data.data)
+training_tf = countVectorizer.fit_transform(training_data.data)
 
 tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_tf)
-X_train_tfidf.shape
+training_tfidf = tfidf_transformer.fit_transform(training_tf)
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn import metrics
-
+#----------------------------------------------------
 multinomial_nb = MultinomialNB()
-multinomial_nb.fit(X_train_tfidf, training_data.target)
-
-X_test_tf = countVectorizer.transform(testing_data.data)
-X_test_tfidf = tfidf_transformer.transform(X_test_tf)
-predicted = multinomial_nb.predict(X_test_tfidf)
+multinomial_nb.fit(training_tfidf, training_data.target)
+#----------------------------------------------------
+testing_tf = countVectorizer.transform(testing_data.data)
+testing_tfidf = tfidf_transformer.transform(testing_tf)
+predicted = multinomial_nb.predict(testing_tfidf)
 
 print("Accuracy:", accuracy_score(testing_data.target, predicted))
 print(classification_report(testing_data.target,predicted, target_names=testing_data.target_names))
