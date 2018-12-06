41be87b493d43a8e08806a7af38133e3de155c60
Nishanth BS <nishanth.star1@gmail.com>
2018-12-07 00:13:38 +0530

    Backpropogation

--- 4-ann-backpropogation/backpropogation.py
+++ 4-ann-backpropogation/backpropogation.py
@@ -7,23 +7,23 @@ def sigmoid(x):
     return 1/(1+np.exp(-x)) 
 def derivative_sigmoid(x):
     return x*(1-x)
-epoch=1
+epoch=20
 learning_rate = 0.5
 iplayerNeurons = 2 
 hiddenlayerNeurons=3 
 oplayerNeurons = 1 
 hiddenlayerWt=np.random.uniform(size=(iplayerNeurons,
                                             hiddenlayerNeurons)) 
-hiddenlayer_bias = np.random.uniform(size=(1, hiddenlayerNeurons)) 
-outputlayer_weights=np.random.uniform(size= (hiddenlayerNeurons,
+hiddenlayerBias = np.random.uniform(size=(1, hiddenlayerNeurons)) 
+oplayerWt=np.random.uniform(size= (hiddenlayerNeurons,
                                              oplayerNeurons ))  
 outputlayer_bias = np.random.uniform(size=(1, oplayerNeurons)) 
 for i in range(epoch):
-    hiddenlayerIp = np.dot(ipData, hiddenlayerWt) 
-    hiddenlayerIp = hiddenlayerIp + hiddenlayer_bias 
+    hiddenlayerIp = np.dot(ipData, hiddenlayerWt) + hiddenlayerBias 
+    #hiddenlayerIp = hiddenlayerIp + 
     hiddenlayerOp = sigmoid(hiddenlayerIp) 
     
-    outputlayerIp = np.dot(hiddenlayerOp, outputlayer_weights) 
+    outputlayerIp = np.dot(hiddenlayerOp, oplayerWt) 
     outputlayerIp = outputlayerIp + outputlayer_bias
     outputlayerOp=sigmoid(outputlayerIp)
      
@@ -31,13 +31,15 @@ for i in range(epoch):
     outputlayerGradient=derivative_sigmoid(outputlayerOp) 
     outputlayerErr_correction = outputlayerErr* outputlayerGradient
    
-    hiddenlayer_error = outputlayerErr_correction.dot (outputlayer_weights. T) 
+    hiddenlayer_error = outputlayerErr_correction.dot (oplayerWt. T) 
     hiddenlayer_gradient=derivative_sigmoid(hiddenlayerOp) 
     hiddenlayer_error_correction=hiddenlayer_error*hiddenlayer_gradient
     
-    outputlayer_weights += hiddenlayerOp.T.dot(outputlayerErr_correction) * learning_rate 
+    oplayerWt += hiddenlayerOp.T.dot(outputlayerErr_correction) * learning_rate 
     hiddenlayerWt += ipData.T.dot(hiddenlayer_error_correction)*learning_rate
+    print(i,"th iteration ",hiddenlayerWt)
     
 print("Input : ", ipData)
 print("Expected Output :", expectedOp)
 print("actual output: ",outputlayerOp)
+
